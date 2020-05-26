aa9061fce7c7c37acb612a1cc43dd5bee8acab09
Masamitsu Misono <misono@nvsupport.org>
2020-05-26 16:04:59 +0900

    サウンド素材の追加。分設定をコンボボックスに。秒設定を追加

--- js/timer.js
+++ js/timer.js
@@ -1,9 +1,9 @@
 let timer;
 let remaining = 0;
 
-function sound(msg) {
+function sound(fname, msg) {
   let audio = new Audio();
-  audio.src = "sound.mp3";
+  audio.src = fname;
   audio.onended = function() {
     window.speechSynthesis.speak(new SpeechSynthesisUtterance(msg));
   };
@@ -11,7 +11,7 @@ function sound(msg) {
 }
 
 function testSound() {
-  sound("音声テストです");
+  sound("sound.mp3", "音声テストです");
 }
 
 function startTimer() {
@@ -19,7 +19,10 @@ function startTimer() {
   startButton.disabled = true;
 
   const minutes = parseInt(document.getElementById('minutes').value);
-  remainingSeconds = minutes*60;
+  const seconds = parseInt(document.getElementById('seconds').value);
+  remainingSeconds = minutes*60+seconds;
+
+  sound("gong-played1.mp3", "");
 
   timer = setInterval(async () => {
     update();
@@ -35,15 +38,20 @@ function update() {
   remaining.innerHTML = "残り" + minutes + "分" + seconds + "秒";
 
   if (remainingSeconds <= 0) {
-    sound("終了です");
+    sound("gong-played2.mp3", "終了です");
+
     const startButton = document.getElementById('start');
     startButton.disabled = false;
     clearInterval(timer);
 
   } else {
     if (seconds == 59) {
-      sound("残り時間" + (minutes + 1) + "分です");
+      sound("sound.mp3", "残り時間" + (minutes + 1) + "分です");
     }
-  }
+    if (seconds == 30) {
+      sound("sound.mp3", (seconds) + "秒経過");
+    }
+  
+}
 }
 
